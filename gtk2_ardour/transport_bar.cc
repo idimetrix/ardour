f2eda045f9b28055e3793b2118985ad03cbccfb4
Ben Loftis <ben@harrisonconsoles.com>
2024-09-24 11:30:19 -0500

    initial implementation of TransportBar

---
+++ gtk2_ardour/transport_bar.cc
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2005-2007 Doug McLain <doug@nostar.net>
+ * Copyright (C) 2005-2017 Tim Mayberry <mojofunk@gmail.com>
+ * Copyright (C) 2005-2019 Paul Davis <paul@linuxaudiosystems.com>
+ * Copyright (C) 2005 Karsten Wiese <fzuuzf@googlemail.com>
+ * Copyright (C) 2005 Taybin Rutkin <taybin@taybin.com>
+ * Copyright (C) 2006-2015 David Robillard <d@drobilla.net>
+ * Copyright (C) 2007-2012 Carl Hetherington <carl@carlh.net>
+ * Copyright (C) 2008-2010 Sakari Bergen <sakari.bergen@beatwaves.net>
+ * Copyright (C) 2012-2019 Robin Gareus <robin@gareus.org>
+ * Copyright (C) 2013-2015 Colin Fletcher <colin.m.fletcher@googlemail.com>
+ * Copyright (C) 2013-2016 John Emmas <john@creativepost.co.uk>
+ * Copyright (C) 2013-2016 Nick Mainsbridge <mainsbridge@gmail.com>
+ * Copyright (C) 2014-2024 Ben Loftis <ben@harrisonconsoles.com>
+ * Copyright (C) 2015 André Nusser <andre.nusser@googlemail.com>
+ * Copyright (C) 2016-2018 Len Ovens <len@ovenwerks.net>
+ * Copyright (C) 2017 Johannes Mueller <github@johannes-mueller.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifdef WAF_BUILD
+#include "gtk2ardour-config.h"
+#include "gtk2ardour-version.h"
+#endif
+
+#include <glib.h>
+#include "pbd/gstdio_compat.h"
+
+#include <gtkmm/accelmap.h>
+#include <gtkmm/messagedialog.h>
+#include <gtkmm/stock.h>
+#include <gtkmm/uimanager.h>
+
+#include "pbd/error.h"
+#include "pbd/compose.h"
+#include "pbd/convert.h"
+#include "pbd/failed_constructor.h"
+#include "pbd/memento_command.h"
+#include "pbd/openuri.h"
+#include "pbd/types_convert.h"
+#include "pbd/file_utils.h"
+#include <pbd/localtime_r.h>
+#include "pbd/pthread_utils.h"
+#include "pbd/replace_all.h"
+#include "pbd/scoped_file_descriptor.h"
+#include "pbd/xml++.h"
+
+#include "gtkmm2ext/application.h"
+#include "gtkmm2ext/bindings.h"
+#include "gtkmm2ext/gtk_ui.h"
+#include "gtkmm2ext/utils.h"
+#include "gtkmm2ext/window_title.h"
+
+#include "widgets/tooltips.h"
+
+#include "ardour/ardour.h"
+#include "ardour/audioengine.h"
+#include "ardour/profile.h"
+#include "ardour/revision.h"
+#include "ardour/transport_master.h"
+#include "ardour/transport_master_manager.h"
+#include "ardour/triggerbox.h"
+#include "ardour/track.h"
+#include "ardour/vca_manager.h"
+#include "ardour/utils.h"
+
+#include "control_protocol/basic_ui.h"
+
+#include "actions.h"
+#include "ardour_ui.h"
+#include "debug.h"
+#include "gui_object.h"
+#include "gui_thread.h"
+#include "keyboard.h"
+#include "keyeditor.h"
+#include "luainstance.h"
+#include "main_clock.h"
+#include "meter_patterns.h"
+#include "mixer_ui.h"
+#include "public_editor.h"
+#include "rc_option_editor.h"
+#include "recorder_ui.h"
+#include "session_dialog.h"
+#include "session_option_editor.h"
+#include "splash.h"
+#include "time_info_box.h"
+#include "timers.h"
+#include "transport_bar.h"
+#include "trigger_page.h"
+#include "triggerbox_ui.h"
+#include "utils.h"
+
+#include "pbd/i18n.h"
+
+using namespace ARDOUR;
+using namespace ARDOUR_UI_UTILS;
+using namespace PBD;
+using namespace Gtkmm2ext;
+using namespace ArdourWidgets;
+using namespace Gtk;
+using namespace std;
+
+static const gchar *_record_mode_strings[] = {
+	N_("Layered"),
+	N_("Non-Layered"),
+	N_("Snd on Snd"),
+	0
+};
+
+TransportBar::TransportBar ()
+	: _basic_ui (0)
+{
+	transport_ctrl.setup (ARDOUR_UI::instance ());
+	transport_ctrl.map_actions ();
+
+	pack_start(transport_ctrl, false, false);
+}
+
+TransportBar::~TransportBar ()
+{
+}
+
+void
+TransportBar::set_session (Session *s)
+{
+	SessionHandlePtr::set_session (s);
+
+	transport_ctrl.set_session (s);
+
+	if (_basic_ui) {
+		delete _basic_ui;
+	}
+
+	_basic_ui = new BasicUI (*s);
+}
---
+++ gtk2_ardour/transport_bar.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2005-2024 Paul Davis <paul@linuxaudiosystems.com>
+ * Copyright (C) 2005 Karsten Wiese <fzuuzf@googlemail.com>
+ * Copyright (C) 2005 Taybin Rutkin <taybin@taybin.com>
+ * Copyright (C) 2006-2007 Doug McLain <doug@nostar.net>
+ * Copyright (C) 2006-2011 David Robillard <d@drobilla.net>
+ * Copyright (C) 2007-2012 Carl Hetherington <carl@carlh.net>
+ * Copyright (C) 2007-2015 Tim Mayberry <mojofunk@gmail.com>
+ * Copyright (C) 2008 Hans Baier <hansfbaier@googlemail.com>
+ * Copyright (C) 2012-2015 Colin Fletcher <colin.m.fletcher@googlemail.com>
+ * Copyright (C) 2013-2015 Nick Mainsbridge <mainsbridge@gmail.com>
+ * Copyright (C) 2013-2016 John Emmas <john@creativepost.co.uk>
+ * Copyright (C) 2013-2019 Robin Gareus <robin@gareus.org>
+ * Copyright (C) 2014-2024 Ben Loftis <ben@harrisonconsoles.com>
+ * Copyright (C) 2017 Johannes Mueller <github@johannes-mueller.org>
+ * Copyright (C) 2018 Len Ovens <len@ovenwerks.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#pragma once
+
+#include <gtkmm/box.h>
+#include <gtkmm/label.h>
+#include <gtkmm/table.h>
+
+#include "ardour/ardour.h"
+#include "ardour/types.h"
+#include "ardour/session_handle.h"
+
+#include "widgets/ardour_button.h"
+#include "widgets/ardour_dropdown.h"
+#include "widgets/ardour_spacer.h"
+
+#include "mini_timeline.h"
+#include "shuttle_control.h"
+#include "startup_fsm.h"
+#include "transport_control.h"
+#include "transport_control_ui.h"
+#include "visibility_group.h"
+#include "window_manager.h"
+
+class BasicUI;
+class TimeInfoBox;
+class LevelMeterHBox;
+
+namespace ARDOUR {
+	class Route;
+	class RouteGroup;
+}
+
+class TransportBar : public Gtk::HBox, public ARDOUR::SessionHandlePtr
+{
+public:
+	TransportBar ();
+	~TransportBar();
+
+	void set_session (ARDOUR::Session *);
+
+private:
+	BasicUI*      _basic_ui;
+
+	TransportControlUI transport_ctrl;
+};
